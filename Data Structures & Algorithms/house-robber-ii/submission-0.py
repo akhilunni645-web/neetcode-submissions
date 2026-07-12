b4449a832c5cf157bdcb0a2f5ea63e2f3c4afcd7
Akhil S <akhilunni645@gmail.com>
2026-07-12 23:53:22 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+
+    
+        if len(nums) == 1:
+            return nums[0]
+
+        def robLine(houses):
+
+            rob1 = 0
+            rob2 = 0
+
+            for money in houses:
+
+                temp = max(money + rob1, rob2)
+
+                rob1 = rob2
+                rob2 = temp
+
+            return rob2
+
+        return max(
+            robLine(nums[:-1]),
+            robLine(nums[1:])
+        )
